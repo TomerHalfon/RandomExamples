d9183928463c1bf9ef6d0090fe5099df07949bfb
TomerHalfon <tomer.halfon773@gmail.com>
2020-08-28 16:10:21 +0300

    Added a delegate examp

---
+++ UpdateUIFromLibraryExample/Library/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Library
+{
+    //Create a delegate to which the ui will add methods to change the view of the Amount Property
+    public delegate void UpdatedAmount();
+    public class Manager
+    {
+        //Create a "Suite" of methods wich have the UpdateAmount delegate signiture
+        public UpdatedAmount updatedAmount;
+        decimal _amount = 0;
+        //when ever we change the property we want to update the UI as the UI wants us to, the way to do that is to invoke the "Suite"
+        public decimal Amount { get { return _amount; } private set { _amount = value; updatedAmount?.Invoke(); } }
+        public void UpdateAmount()
+        {
+            Amount += 50;
+        }
+    }
+}
---
+++ UpdateUIFromLibraryExample/UWPUI/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPUI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Manager _manager;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _manager = new Manager();
+            //Register a way of updating the UI the the "suite"
+            _manager.updatedAmount += UpdateUI;
+        }
+        //Here is how we update the UI
+        private void UpdateUI() => MoneyViewBlock.Text = _manager.Amount.ToString("C");
+        //This is when we update the property
+        private void UpdateMoney_Click(object sender, RoutedEventArgs e) => _manager.UpdateAmount();
+    }
+}
